d2e0fc4a5ab983ea9b6367ef21f4f7deb668a5d6
salmaabakil <153354773+salmaabakil@users.noreply.github.com>
2026-01-03 20:29:27 +0100

    DataBase

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 """
-Configuration de la base de données SQLite
+Configuration de la base de données SQLite  
 """
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
